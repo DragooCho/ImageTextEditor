7b4e7f1767bc678e4ffb2a2db99d0041364d1f5d
dragoocho <deagoocho@gmail.com>
2021-03-02 22:59:41 +0900

    폰트크기 조절구현작업

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let canvas;
 let img;
 
 function setup() {
-  canvas = createCanvas(1000, 1000);
+  canvas = createCanvas(300, 300);
   canvas.drop(gotFile);
   canvas.mousePressed(inputTextPaint);
   background(180, 0, 180);
@@ -18,8 +18,8 @@ function imageView() {
 }
 
 function inputTextPaint() {
-  textSize(25); // 사이즈 조절
+  textSize(fontSizeEvent); // 사이즈 조절
   textAlign(CENTER, CENTER);
-  textStyle(BOLD); // 스타일 지정, NORMAL, ITALIC, BOLD, BOLDITALIC 를 설정 가능
-  text(fontInput, mouseX, mouseY); // 폰트의 내용, 해당 마우스 위치에 그려준다.
+  textStyle(BOLDITALIC); // 스타일 지정, NORMAL, ITALIC, BOLD, BOLDITALIC 를 설정 가능
+  text(inputTextEvent, mouseX, mouseY); // 폰트의 내용, 해당 마우스 위치에 그려준다.
 }
--- utilities.js
+++ utilities.js
@@ -1,1 +1,20 @@
-const fontInput = "Font Size";
+const textInput = document.getElementById("textInput");
+const making = document.getElementById("making");
+const fontChange = document.getElementById("fontChange");
+const fontSize = document.getElementById("fontSize");
+
+let inputTextEvent, fontSizeEvent;
+
+textInput.addEventListener("change", function (e) {
+  inputTextEvent = e.target.value;
+  console.log(inputTextEvent);
+});
+fontSize.addEventListener("change", function (e) {
+  fontSizeEvent = e.target.value;
+  console.log(fontSizeEvent);
+});
+
+// fontChange.addEventListener("select", function (e) {
+//   fontChangEvent = e.target.value;
+//   console.log("fontChangEvent");
+// });
