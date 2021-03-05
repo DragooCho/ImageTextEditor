b34a347211177d6de7cc076bd4b5059ccf71a5ef
dragoocho <deagoocho@gmail.com>
2021-03-06 03:39:17 +0900

    폰트 조정

--- sketch.js
+++ sketch.js
@@ -1,4 +1,10 @@
-let img, input, button;
+let img, input, button, textSizeSlider, dropdown, koFont;
+
+function preload() {
+  koFont = loadFont(
+    "https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_three@1.0/SangSangFlowerRoad.woff"
+  );
+}
 
 function setup() {
   let c = createCanvas(500, 500);
@@ -11,20 +17,35 @@ function setup() {
   );
 
   c.drop(gotFile);
+  input = createFileInput(gotFile);
 
   input = createInput();
   button = createButton("다운로드");
   button.mousePressed(imgDownload);
+
+  textSizeSlider = createSlider(10, 55, 30);
+
+  dropdown = createSelect();
+  // Set options
+  dropdown.option("white");
+  dropdown.option("orange");
+  dropdown.option("green");
+  dropdown.option("skyblue");
+  dropdown.option("pink");
+  dropdown.option("black");
 }
 
 function draw() {
+  const sliderValue = textSizeSlider.value();
+
   if (img) {
     image(img, 0, 0, width, height);
   }
 
-  textSize(32);
+  fill(dropdown.value());
+  textSize(sliderValue);
   textAlign(CENTER, CENTER);
-  textStyle(ITALIC);
+  textFont(koFont);
   text(input.value(), mouseX, mouseY);
 }
 
